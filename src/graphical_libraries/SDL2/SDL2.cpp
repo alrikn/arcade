7abbe9db062800a49125d36062b48f360fef6c40
JadBGHIEL <jad.bghiel@epitech.eu>
2026-04-11 16:00:52 +0200

    Add: init file for sdl implementation

--- src/graphical_libraries/SDL2/SDL2.cpp
+++ src/graphical_libraries/SDL2/SDL2.cpp
@@ -4,3 +4,13 @@
 ** File description:
 ** SDL2
 */
+
+#include "SDL2.hpp"
+#include "Error.hpp"
+#include <SDL2/SDL_image.h>
+
+// check if keycode is a printable ascii char for name input
+static bool isPrintableAscii(SDL_Keycode key)
+{
+	return key >= 32 && key <= 126;
+}
